5590e472e0bb12e6bb677b1cacbb2af0dd342c7e
Anurag Dalmia <anu@Anurags-MBP.lan>
2019-11-12 23:27:15 +0100

    added pie chart

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -1,23 +1,85 @@
 import React,{useState} from 'react';
 import './Drawer.css';
-import Chartkick, { PieChart } from 'react-chartkick'
-import 'chart.js'
+import { ResponsivePie } from '@nivo/pie'
 
-Chartkick.options = {
-  colors: ["#00b383", "#00805e", "#33ffc9"],
-  
-}
+const MyResponsivePie = ({ total}) => (
+  <ResponsivePie
+    data={[{ "id": "Carbs(g)",
+    "label": "Carbohydrate",
+    "value": Number(total.carb.toFixed(1)),
+      "color": "hsl(10, 70%, 50%)"
+    },
+      {
+        "id": "Fat(g)",
+    "label": "Fat",
+    "value": Number(total.fat.toFixed(1)),
+        "color": "hsl(206, 70%, 50%)"
+      },
+      {
+        "id": "Protein(g)",
+    "label": "Protein ",
+    "value": Number(total.pro.toFixed(1)),
+        "color": "hsl(206, 70%, 50%)"
+      }
+    ]}
+      margin={{ top: 50, right: 80, bottom:150, left: 80 }}
+      startAngle={-162}
+      innerRadius={0.5}
+      padAngle={0.7}
+      cornerRadius={3}
+      colors={{ scheme: 'nivo' }}
+      borderWidth={1}
+      borderColor="none"
+      radialLabel="value"
+      radialLabelsSkipAngle={10}
+      radialLabelsTextXOffset={6}
+      radialLabelsTextColor="#000"
+      radialLabelsLinkOffset={1}
+      radialLabelsLinkDiagonalLength={10}
+      radialLabelsLinkHorizontalLength={10}
+      radialLabelsLinkStrokeWidth={2}
+      radialLabelsLinkColor={{ from: 'color' }}
+      slicesLabelsSkipAngle={10}
+      slicesLabelsTextColor="none"
+      animate={true}
+      motionStiffness={90}
+      motionDamping={15}
+      legends={[
+          {
+              anchor: 'top',
+              direction: 'row',
+              translateY: -30,
+              translateX:25,
+              itemWidth: 100,
+              itemHeight: 25,
+              itemTextColor: '#000',
+              symbolSize: 25,
+              symbolShape: 'circle',
+              effects: [
+                  {
+                      on: 'hover',
+                      style: {
+                          itemTextColor: '#000'
+                      }
+                  }
+              ]
+          }
+      ]}
+  />
+)
+
+
+
 const DrawerContents = ({total }) => (
   <div className="DrawerContents__Container">
     <h2>Total Calories : {total.cal}</h2>
     <br />
     <br/>
-    <div className='content'>
-      {/* <h4>Total Carbs: {(total.carb).toFixed(2) + 'g'}</h4>
+    {/* <div className='content'>
+      <h4>Total Carbs: {(total.carb).toFixed(2) + 'g'}</h4>
       <h4>Total Carbs: {(total.fat).toFixed(2) + 'g'}</h4>
-      <h4>Total Carbs: {(total.pro).toFixed(2) + 'g'}</h4> */}
-      <PieChart legend={false} dataset={{borderWidth: 0}} data={[["Carbs", total.carb.toFixed(2)], ["Fat",total.fat.toFixed(2)], ["Protien",total.pro.toFixed(2)]]} />
-    </div>
+      <h4>Total Carbs: {(total.pro).toFixed(2) + 'g'}</h4>
+    </div> */}
   </div>
 );
 
@@ -37,7 +99,8 @@ export default function Total({ total }) {
       <div className={`Drawer-Container ${isOpen && "Drawer-Container-isOpen"}`}>
         <button onClick={toggleDrawer} className='emoji'><span role='img' aria-label="sheep"  > ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯ </span></button>
         <DrawerContents total={total}></DrawerContents>
-    </div>
+        <div className='chart'> <MyResponsivePie total={total}></MyResponsivePie></div>
+      </div>
     </div>
   </div>
   );
